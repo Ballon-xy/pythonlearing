82718447d7efa36a23dc2c9382e2688b03506284
root <root@Z-Laptop.localdomain>
2025-03-09 18:02:45 +0800

    I add some illustrations to every line

--- ex6.py
+++ ex6.py
@@ -1,22 +1,22 @@
-types_of_people = 10
-x =  f"There are { types_of_people} types of people."
+types_of_people = 10 # assign a value to the variable on the left side of the equal sign
+x =  f"There are { types_of_people} types of people." # set the variable as a string
 
-binary = "binary"
-do_not = "don't"
-y = f"Those who know {binary} and those who {do_not}."
+binary = "binary" # set the variable as a string
+do_not = "don't"  # set the variable as a string
+y = f"Those who know {binary} and those who {do_not}." # set the variable as a string
 
-print(x)
-print(y)
+print(x) # print the string
+print(y) # print the string
 
-print(f"I said: {x}")
-print(f"I also said:'{y}'")
+print(f"I said: {x}") # print the string
+print(f"I also said:'{y}'") # print the string
 
-hilarious = False
-joke_evaluation = "Isn't that joke so funny?! {}"
+hilarious = False # assign a value to the variable 
+joke_evaluation = "Isn't that joke so funny?! {}" # set the variable as a string
 
-print(joke_evaluation.format(hilarious))
+print(joke_evaluation.format(hilarious)) # print the string
 
-w = "This is the left side of..."
-e = "a string with a right sied."
-
-print(w + e)
+w = "This is the left side of..." # set the variable as a string
+e = "a string with a right sied." # set the variable as a string
+ 
+print(w + e)#set the variable as a string
